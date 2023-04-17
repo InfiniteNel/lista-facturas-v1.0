334e21e75a9149fe39ce23c042a1785a66c8388a
InfiniteNel <juanrolaGame@gmail.com>
2023-04-17 08:30:54 +0200

    New Version

--- app/src/main/java/com/jroslar/listafacturasv01/ui/MainActivity.kt
+++ app/src/main/java/com/jroslar/listafacturasv01/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jroslar.listafacturasv01.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
